3835e5c9705ce25343548644a4e43648521df46e
Ingrid Portilho da Silva <ingrid.portilho@lmcorp.local>
2022-03-07 15:51:52 -0300

    ola

--- callBack.js
+++ callBack.js
@@ -1,7 +1,12 @@
 const nomes = ['Ana','Ju', 'Leo', 'Paula']
 
-nomes.forEach(imprimeNomes, indice)
+nomes.forEach(imprimeNomes)
 //param b é opcional
-function imprimeNomes(nome){
-    console.log(nome + indice)
+function imprimeNomes(nome, indice){
+    console.log(nome + ' - ' + indice)
 }
+
+//CallBack: é uma função que é executada
+//após o processamento de outra função.
+//É quando uma função é passada como 
+//argumento de outra.
---
+++ map-nomes.js
@@ -0,0 +1,5 @@
+let nomes = ['ana Julia', 'Caio vinicius', 'BIA silva']
+
+const nomesAt = nomes.map(nome =>  nome.toUpperCase())
+
+console.log(nomesAt)
